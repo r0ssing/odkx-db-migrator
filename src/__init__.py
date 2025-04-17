1e45956673f8b33486629981848b98973cf25c88
Emil Rossing <emil@rossing.org>
2025-04-17 16:37:28 +0200

    Updated package exports

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,11 @@
 """
-SQLite Data Migration Tool
-""" 
+ODK-X Database Migration Tool
+
+A tool for migrating data between ODK-X databases with different schemas,
+supporting pseudotype conversions and attachment management.
+"""
+
+from .migrator import DatabaseMigrator
+from .models import DatabaseConfig
+
+__all__ = ['DatabaseMigrator', 'DatabaseConfig']
